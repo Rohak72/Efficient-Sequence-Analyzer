4c937a4a540b28e7ac2e0b58b21f3da991085638
Rohak72 <rohakjain@gmail.com>
2025-08-26 11:41:04 -0700

    Redo AWS helper functions to include S3, SQS, and Redis.

---
+++ backend/app/scripts/aws_tools.py
@@ -0,0 +1,79 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import boto3
+import json
+import redis
+import os
+import io
+
+fasta_bucket_name = os.environ.get("FASTA_S3_BUCKET_NAME")
+sqs_queue_url = os.environ.get("JOB_QUEUE_URL")
+sqs_client = boto3.client("sqs")
+
+redis_client = redis.Redis(
+    host=os.environ.get("REDIS_HOST", "localhost"),
+    port=int(os.environ.get("REDIS_PORT", 6379)),
+    decode_responses=True)
+
+# In Lambda, boto3 will automatically find the IAM Role credentials -- no keys needed.
+# However, when running locally, we need to collect the credentials ourselves.
+if os.environ.get("AWS_LAMBDA_FUNCTION_NAME"):
+    print("Running in Lambda, using IAM Role for S3 credentials.")
+    s3_client = boto3.client("s3", region_name="us-east-2")
+else:
+    print("Running locally, using .env file for S3 credentials.")
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name="us-east-2")
+
+# S3 Helpers
+
+async def upload_to_s3(file_obj, upload_key: str):
+    raw_content = await file_obj.read()
+    s3_client.put_object(Bucket=fasta_bucket_name, Key=upload_key, Body=raw_content)
+
+def download_from_s3(file_key: str):
+    file_obj = s3_client.get_object(Bucket=fasta_bucket_name, Key=file_key)
+    file_content = file_obj["Body"].read().decode("utf-8")
+    return io.StringIO(file_content) 
+
+def get_bucket_name():
+    return fasta_bucket_name
+
+def generate_presigned_url(key: str, filename: str = None, expires: int = 3600):
+    url_params = {'Bucket': fasta_bucket_name, 'Key': key}
+    if filename:
+        url_params['ResponseContentDisposition'] = f'attachment; filename="{filename}"'
+
+    return s3_client.generate_presigned_url('get_object', Params=url_params, ExpiresIn=expires)
+
+# SQS Helper
+
+async def enqueue_job(job_id, input_key, target_key, direction, user_id=None):
+    message = {
+        "job_id": job_id,
+        "input_key": input_key,
+        "target_key": target_key,
+        "direction": direction,
+        "user_id": user_id
+    }
+    sqs_client.send_message(QueueUrl=sqs_queue_url, MessageBody=json.dumps(message))
+    set_job_status(job_id, "PENDING")
+
+# Redis Helpers
+
+def set_job_status(job_id, status, result_key=None):
+    data = {"status": status}
+    if result_key:
+        data["result_key"] = result_key
+    redis_client.hset(job_id, data)
+
+def get_job_status(job_id):
+    status = redis_client.hgetall(job_id)
+    return status if status else {'status': 'UNKNOWN'}
